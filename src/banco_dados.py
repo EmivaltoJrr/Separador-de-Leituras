f7f7b803c0996d18f1cbd1f4112aadf37f3d982d
Emivalto da Costa Tavares Junior <emivalto14@gmail.com>
2026-09-24 12:35:24 -0300

    feat: add Catholic Bible book database and reading list files

--- src/banco_dados.py
+++ src/banco_dados.py
@@ -2,48 +2,53 @@
 ARQUIVO_MODELO = "leituras.docx"
 ARQUIVO_SAIDA = "leituras_preenchidas.docx"
 
-# --- MAPEAMENTO DA BÍBLIA ---
+# --- MAPEAMENTO DA BÍBLIA CATÓLICA (ESTRUTURA SOLICITADA) ---
 LIVROS_DB = {
-    # HISTÓRICOS
-    'gn': ('Gênesis', 'Históricos'), 'ex': ('Êxodo', 'Históricos'), 
-    'lv': ('Levítico', 'Históricos'), 'nm': ('Números', 'Históricos'), 
-    'dt': ('Deuteronômio', 'Históricos'), 'js': ('Josué', 'Históricos'), 
-    'jz': ('Juízes', 'Históricos'), 'rt': ('Rute', 'Históricos'), 
-    '1sm': ('1 Samuel', 'Históricos'), '2sm': ('2 Samuel', 'Históricos'), 
-    '1rs': ('1 Reis', 'Históricos'), '2rs': ('2 Reis', 'Históricos'), 
-    '1cr': ('1 Crônicas', 'Históricos'), '2cr': ('2 Crônicas', 'Históricos'), 
-    'es': ('Esdras', 'Históricos'), 'ne': ('Neemias', 'Históricos'), 
-    'tb': ('Tobias', 'Históricos'), 'jt': ('Judite', 'Históricos'), 
-    'est': ('Ester', 'Históricos'), '1mc': ('1 Macabeus', 'Históricos'), 
-    '2mc': ('2 Macabeus', 'Históricos'), 'at': ('Atos dos Apóstolos', 'Históricos'),
-    # PROFETAS
-    'jo': ('Jó', 'Profetas'), 'job': ('Jó', 'Profetas'),
-    'sl': ('Salmos', 'Profetas'), 'pv': ('Provérbios', 'Profetas'), 
-    'ecl': ('Eclesiastes', 'Profetas'), 'ct': ('Cântico dos Cânticos', 'Profetas'), 
-    'sb': ('Sabedoria', 'Profetas'), 'eclo': ('Eclesiástico', 'Profetas'), 
-    'is': ('Isaías', 'Profetas'), 'jr': ('Jeremias', 'Profetas'), 
-    'lm': ('Lamentações', 'Profetas'), 'br': ('Baruc', 'Profetas'), 
-    'ez': ('Ezequiel', 'Profetas'), 'dn': ('Daniel', 'Profetas'), 
-    'os': ('Oseias', 'Profetas'), 'jl': ('Joel', 'Profetas'), 
-    'am': ('Amós', 'Profetas'), 'ab': ('Abdias', 'Profetas'), 
-    'jn': ('Jonas', 'Profetas'), 'mq': ('Miqueias', 'Profetas'), 
-    'na': ('Naum', 'Profetas'), 'hc': ('Habacuc', 'Profetas'), 
-    'sf': ('Sofonias', 'Profetas'), 'ag': ('Ageu', 'Profetas'), 
-    'zc': ('Zacarias', 'Profetas'), 'ml': ('Malaquias', 'Profetas'),
-    # EVANGELHO
-    'mt': ('Mateus', 'Evangelho'), 'mc': ('Marcos', 'Evangelho'), 
-    'lc': ('Lucas', 'Evangelho'), 'joao': ('João', 'Evangelho'),
-    # CARTAS
-    'rm': ('Romanos', 'Cartas'), '1cor': ('1 Coríntios', 'Cartas'), 
-    '2cor': ('2 Coríntios', 'Cartas'), 'gl': ('Gálatas', 'Cartas'), 
-    'ef': ('Efésios', 'Cartas'), 'fp': ('Filipenses', 'Cartas'), 
-    'fl': ('Filipenses', 'Cartas'), 'cl': ('Colossenses', 'Cartas'), 
-    '1ts': ('1 Tessalonicenses', 'Cartas'), '2ts': ('2 Tessalonicenses', 'Cartas'), 
-    '1tm': ('1 Timóteo', 'Cartas'), '2tm': ('2 Timóteo', 'Cartas'), 
-    'tt': ('Tito', 'Cartas'), 'fm': ('Filemom', 'Cartas'), 
-    'hb': ('Hebreus', 'Cartas'), 'tg': ('Tiago', 'Cartas'), 
-    '1pd': ('1 Pedro', 'Cartas'), '2pd': ('2 Pedro', 'Cartas'), 
-    '1jo': ('1 João', 'Cartas'), '2jo': ('2 João', 'Cartas'), 
-    '3jo': ('3 João', 'Cartas'), 'jd': ('Judas', 'Cartas'), 
-    'ap': ('Apocalipse', 'Cartas')
+    # --- HISTÓRICOS ---
+    'gn': ('Gênesis', 'HISTÓRICOS'), 'ex': ('Êxodo', 'HISTÓRICOS'), 
+    'lv': ('Levítico', 'HISTÓRICOS'), 'nm': ('Números', 'HISTÓRICOS'), 
+    'dt': ('Deuteronômio', 'HISTÓRICOS'), 'js': ('Josué', 'HISTÓRICOS'), 
+    'jz': ('Juízes', 'HISTÓRICOS'), 'rt': ('Rute', 'HISTÓRICOS'), 
+    '1sm': ('1 Samuel', 'HISTÓRICOS'), '2sm': ('2 Samuel', 'HISTÓRICOS'), 
+    '1rs': ('1 Reis', 'HISTÓRICOS'), '2rs': ('2 Reis', 'HISTÓRICOS'), 
+    '1cr': ('1 Crônicas', 'HISTÓRICOS'), '2cr': ('2 Crônicas', 'HISTÓRICOS'), 
+    'es': ('Esdras', 'HISTÓRICOS'), 'ne': ('Neemias', 'HISTÓRICOS'), 
+    'tb': ('Tobias', 'HISTÓRICOS'), 'jt': ('Judite', 'HISTÓRICOS'), 
+    'est': ('Ester', 'HISTÓRICOS'), '1mc': ('1 Macabeus', 'HISTÓRICOS'), 
+    '2mc': ('2 Macabeus', 'HISTÓRICOS'), 'at': ('Atos dos Apóstolos', 'HISTÓRICOS'),
+
+    # --- PROFETAS 
+    'jo': ('Jó', 'PROFETAS'), 'job': ('Jó', 'PROFETAS'),
+    'sl': ('Salmos', 'PROFETAS'), 'pv': ('Provérbios', 'PROFETAS'), 
+    'ecl': ('Eclesiastes', 'PROFETAS'), 'ct': ('Cântico dos Cânticos', 'PROFETAS'), 
+    'sb': ('Sabedoria', 'PROFETAS'), 'eclo': ('Eclesiástico', 'PROFETAS'), 
+    'sir': ('Eclesiástico', 'PROFETAS'), 'is': ('Isaías', 'PROFETAS'), 
+    'jr': ('Jeremias', 'PROFETAS'), 'lm': ('Lamentações', 'PROFETAS'), 
+    'br': ('Baruc', 'PROFETAS'), 'ez': ('Ezequiel', 'PROFETAS'), 
+    'dn': ('Daniel', 'PROFETAS'), 'os': ('Oseias', 'PROFETAS'), 
+    'jl': ('Joel', 'PROFETAS'), 'am': ('Amós', 'PROFETAS'), 
+    'ab': ('Abdias', 'PROFETAS'), 'jn': ('Jonas', 'PROFETAS'), 
+    'mq': ('Miqueias', 'PROFETAS'), 'na': ('Naum', 'PROFETAS'), 
+    'hc': ('Habacuc', 'PROFETAS'), 'sf': ('Sofonias', 'PROFETAS'), 
+    'ag': ('Ageu', 'PROFETAS'), 'zc': ('Zacarias', 'PROFETAS'), 
+    'ml': ('Malaquias', 'PROFETAS'),
+
+    # --- EVANGELHO ---
+    'mt': ('Mateus', 'EVANGELHO'), 'mc': ('Marcos', 'EVANGELHO'), 
+    'lc': ('Lucas', 'EVANGELHO'), 'joao': ('João', 'EVANGELHO'), 
+    'jo': ('João', 'EVANGELHO'), 
+
+    # --- CARTAS ---
+    'rm': ('Romanos', 'CARTAS'), '1cor': ('1 Coríntios', 'CARTAS'), 
+    '2cor': ('2 Coríntios', 'CARTAS'), 'gl': ('Gálatas', 'CARTAS'), 
+    'ef': ('Efésios', 'CARTAS'), 'fp': ('Filipenses', 'CARTAS'), 
+    'fl': ('Filipenses', 'CARTAS'), 'cl': ('Colossenses', 'CARTAS'), 
+    '1ts': ('1 Tessalonicenses', 'CARTAS'), '2ts': ('2 Tessalonicenses', 'CARTAS'), 
+    '1tm': ('1 Timóteo', 'CARTAS'), '2tm': ('2 Timóteo', 'CARTAS'), 
+    'tt': ('Tito', 'CARTAS'), 'fm': ('Filemom', 'CARTAS'), 
+    'hb': ('Hebreus', 'CARTAS'), 'tg': ('Tiago', 'CARTAS'), 
+    '1pd': ('1 Pedro', 'CARTAS'), '2pd': ('2 Pedro', 'CARTAS'), 
+    '1jo': ('1 João', 'CARTAS'), '2jo': ('2 João', 'CARTAS'), 
+    '3jo': ('3 João', 'CARTAS'), 'jd': ('Judas', 'CARTAS'), 
+    'ap': ('Apocalipse', 'CARTAS')
 }
